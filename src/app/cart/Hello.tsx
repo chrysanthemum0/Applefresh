a78fc3880abf85db1f1e8526157f0d49906d210d
chrysanthemum0 <wlsghd3241@gmail.com>
2024-06-03 20:49:42 +0900

    update json update import file name : 임포트 할 때 타입스크리트는 확장자 때고 임포트 할 것 ex ) import { age, name } from './data.js'; import Hello from './Hello';

--- src/app/cart/Hello.tsx
+++ src/app/cart/Hello.tsx
@@ -2,6 +2,6 @@ import { age, name } from './data.js';
 
 export default function Hello(){
     return(
-        <h2 className='title'>안녕하세요&nbsp; {name}님&nbsp; 오늘 하루도 좋은 하루 되세요.</h2>
+        <h2 className='title'>안녕하세요.&nbsp; {name}님&nbsp; 오늘 하루도 좋은 하루 되세요.</h2>
     )
 }
